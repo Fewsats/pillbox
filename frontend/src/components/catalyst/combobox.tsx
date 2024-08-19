a6770c3d2d657844bd8d09107b7709abc2a59d9b
Victoria <39459423+VictoriaVM@users.noreply.github.com>
2024-08-19 15:07:45 +0300

    select fix

--- frontend/src/components/catalyst/combobox.tsx
+++ frontend/src/components/catalyst/combobox.tsx
@@ -69,35 +69,37 @@ export const Combobox = forwardRef(function Input(
         {({ open }) => (
           <React.Fragment>
             <div className='relative'>
-              <Headless.ComboboxInput
-                aria-label={name}
-                displayValue={(option: Option) => value?.name}
-                onChange={(event) => setQuery(event.target.value)}
-                className={clsx(
-                  // Basic layout
-                  'relative block w-full appearance-none rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] has-[[data-disabled]]:opacity-50 sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)]',
-                  // Typography
-                  'text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white',
-                  // Border
-                  'border border-zinc-950/10 data-[hover]:border-zinc-950/20 dark:border-white/10 dark:data-[hover]:border-white/20',
-                  // Background color
-                  'bg-transparent dark:bg-white/5',
-                  // Hide default focus styles
-                  'focus:outline-none',
-                  // Invalid state
-                  search
-                    ? 'data-[invalid]:border-red-500 data-[invalid]:data-[hover]:border-red-500 data-[invalid]:dark:border-red-500 data-[invalid]:data-[hover]:dark:border-red-500'
-                    : '',
-                  // Disabled state
-                  search
-                    ? 'data-[disabled]:border-zinc-950/20 dark:data-[hover]:data-[disabled]:border-white/15 data-[disabled]:dark:border-white/15 data-[disabled]:dark:bg-white/[2.5%]'
-                    : '',
-                  // System icons
-                  'dark:[color-scheme:dark]',
-                  !search ? '!text-transparent' : ''
-                )}
-                disabled={!search}
-              />
+              <Headless.ComboboxButton className={'h-full w-full'}>
+                <Headless.ComboboxInput
+                  aria-label={name}
+                  displayValue={(option: Option) => value?.name}
+                  onChange={(event) => setQuery(event.target.value)}
+                  className={clsx(
+                    // Basic layout
+                    'relative block w-full appearance-none rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] has-[[data-disabled]]:opacity-50 sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)]',
+                    // Typography
+                    'text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white',
+                    // Border
+                    'border border-zinc-950/10 data-[hover]:border-zinc-950/20 dark:border-white/10 dark:data-[hover]:border-white/20',
+                    // Background color
+                    'bg-transparent dark:bg-white/5',
+                    // Hide default focus styles
+                    'focus:outline-none',
+                    // Invalid state
+                    search
+                      ? 'data-[invalid]:border-red-500 data-[invalid]:data-[hover]:border-red-500 data-[invalid]:dark:border-red-500 data-[invalid]:data-[hover]:dark:border-red-500'
+                      : '',
+                    // Disabled state
+                    search
+                      ? 'data-[disabled]:border-zinc-950/20 dark:data-[hover]:data-[disabled]:border-white/15 data-[disabled]:dark:border-white/15 data-[disabled]:dark:bg-white/[2.5%]'
+                      : '',
+                    // System icons
+                    'dark:[color-scheme:dark]',
+                    !search ? '!text-transparent' : ''
+                  )}
+                  disabled={!search}
+                />
+              </Headless.ComboboxButton>
               {!search && (
                 <div
                   style={{
